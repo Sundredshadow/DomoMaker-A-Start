34f9622e5eaef2b72d25420808c19201fca3eaf2
sundredshadow <sundredshadow@gmail.com>
2021-04-04 14:16:11 -0400

    Domo C edits for eslint and some error fixing

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
 
-//redis stuff
-const RedisStore=require('connect-redis')(session)
-const url =require('url');
-const redis=require('redis');
-const csrf=require('csurf');
+// redis stuff
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -31,21 +31,21 @@ mongoose.connect(dbURL, mongooseOptions, (err) => {
   }
 });
 
-//redis stuff
-let redisURL={
-  hostname:'redis-18289.c256.us-east-1-2.ec2.cloud.redislabs.com',
-  port:18289,
+// redis stuff
+let redisURL = {
+  hostname: 'redis-18289.c256.us-east-1-2.ec2.cloud.redislabs.com',
+  port: 18289,
 };
 
-let redisPASS='zuowwHRHfKSLmZfbdP4RPuAgemj80SfO';
-if(process.env.REDISCLOUD_URL){
-  redisURL=url.parse(process.env.REDISCLOUD_URL);
-  [,redisPass]=redisURL.auth.split(':');
+let redisPASS = 'zuowwHRHfKSLmZfbdP4RPuAgemj80SfO';
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  [, redisPASS] = redisURL.auth.split(':');
 }
-let redisClient=redis.createClient({
-  host:redisURL.hostname,
-  port:redisURL.port,
-  password:redisPASS,
+const redisClient = redis.createClient({
+  host: redisURL.hostname,
+  port: redisURL.port,
+  password: redisPASS,
 });
 
 
@@ -54,7 +54,6 @@ const router = require('./router.js');
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -63,26 +62,28 @@ app.use(bodyParser.urlencoded({
 app.use(session({
   key: 'sessionid',
   store: new RedisStore({
-    client:redisClient,
+    client: redisClient,
   }),
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
-  cookie:{
-    httpOnly:true,
+  cookie: {
+    httpOnly: true,
   },
 }));
 
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+app.disable('x-powered-by');
 app.use(cookieParser());
 
-//csrf
+// csrf
 app.use(csrf());
-app.use((err,req,res,next)=>{
-  if(err.code!=='EBADCSRFTOKEN') return next(err);
-
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') {
+    return next(err);
+  }
   console.log('Missing CSRF token');
   return false;
 });

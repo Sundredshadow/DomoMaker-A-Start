7de49b54f0f2f9390b7f0ad1e69e21737c3b1457
sundredshadow <sundredshadow@gmail.com>
2021-04-10 20:33:53 -0400

    Completed domo D

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker3';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMakerD';
 
 const mongooseOptions = {
   useNewUrlParser: true,
